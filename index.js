7054768d2c4a5cdd5affc81836c96553779dba13
Rasujon3 <rasujon3@gmail.com>
2022-04-25 02:38:49 +0600

    68-8 server error solve

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ function verifyJWT(req, res, next) {
     }
     console.log("decoded", decoded);
     req.decoded = decoded;
+    next();
   });
   // console.log("inside verifyJWT", authHeader);
-  next();
 }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aq3cg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
